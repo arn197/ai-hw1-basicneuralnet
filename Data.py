4f350b2d19e06117868364c3921cdb0254d50a4c
Rahul Ramachandran <rahulpisharody@gmail.com>
2019-10-09 20:51:33 -0400

    Main Funtion complete

--- Data.py
+++ Data.py
@@ -2,41 +2,49 @@ import numpy as np
 from itertools import permutations
 #%%
 def getData(dataDir):
-    if dataDir[1]=="lin":
-        X = np.loadtxt(dataDir[0]+"/LinearX.csv",delimiter=",")
-        Y = np.loadtxt(dataDir[0]+"/LinearY.csv",delimiter=",")
-    else:
-        X = np.loadtxt(dataDir[0]+"/NonlinearX.csv",delimiter=",")
-        Y = np.loadtxt(dataDir[0]+"/NonlinearY.csv",delimiter=",")
-    assert X.shape[0] == Y.shape[0]
-    return X,Y
+	if dataDir[1]=="lin":
+		X = np.loadtxt(dataDir[0]+"/LinearX.csv",delimiter=",")
+		Y = np.loadtxt(dataDir[0]+"/LinearY.csv",delimiter=",")
+	elif dataDir[1]=="nonlin":
+		X = np.loadtxt(dataDir[0]+"/NonlinearX.csv",delimiter=",")
+		Y = np.loadtxt(dataDir[0]+"/NonlinearY.csv",delimiter=",")
+	else:
+		X,Y=[],[]
+		X.append(np.loadtxt(dataDir[0]+"/Digit_X_train.csv",delimiter=","))
+		X.append(np.loadtxt(dataDir[0]+"/Digit_X_test.csv",delimiter=","))
+		Y.append(np.loadtxt(dataDir[0]+"/Digit_Y_train.csv",delimiter=","))
+		Y.append(np.loadtxt(dataDir[0]+"/Digit_Y_test.csv",delimiter=","))
+		X=np.array(X)
+		Y=np.array(Y)
+	assert X.shape[0] == Y.shape[0]
+	return X,Y
 
 def splitData(X, Y, K = 5):
-    '''
-    Returns
-    -------
-    result : List[[train, test]]
-        "train" is a list of indices corresponding to the training samples in the data.
-        "test" is a list of indices corresponding to the testing samples in the data.
-        For example, if the first list in the result is [[0, 1, 2, 3], [4]], then the 4th
-        sample in the data is used for testing while the 0th, 1st, 2nd, and 3rd samples
-        are for training.
-    '''
-    assert X.shape[0] == Y.shape[0]
+	'''
+	Returns
+	-------
+	result : List[[train, test]]
+		"train" is a list of indices corresponding to the training samples in the data.
+		"test" is a list of indices corresponding to the testing samples in the data.
+		For example, if the first list in the result is [[0, 1, 2, 3], [4]], then the 4th
+		sample in the data is used for testing while the 0th, 1st, 2nd, and 3rd samples
+		are for training.
+	'''
+	assert X.shape[0] == Y.shape[0]
 
-    m = X.shape[0]
-    fold_size = m//K
-    A = np.random.permutation(m).tolist()
-    folds = list()
-    for i in range(K):
-        folds.append(A[i*fold_size:(i+1)*fold_size])
-    K_Split = list()
-    for i in range(len(folds)):
-        l = list()
-        for j in range(len(folds)):
-            if i!=j:
-                l = l+folds[j]
-        K_Split.append([l,folds[i]])
-    K_Split = np.array(K_Split)
-    # print(K_Split[0][0][1])
-    return K_Split
+	m = X.shape[0]
+	fold_size = m//K
+	A = np.random.permutation(m).tolist()
+	folds = list()
+	for i in range(K):
+		folds.append(A[i*fold_size:(i+1)*fold_size])
+	K_Split = list()
+	for i in range(len(folds)):
+		l = list()
+		for j in range(len(folds)):
+			if i!=j:
+				l = l+folds[j]
+		K_Split.append([l,folds[i]])
+	K_Split = np.array(K_Split)
+	# print(K_Split[0][0][1])
+	return K_Split
--- main.py
+++ main.py
@@ -1,33 +1,48 @@
 from NeuralNet_ML import *
 from Data import *
+from ConfusionMatrix import *
 import random
+import pandas as pd
+
+def plotDecisionBoundary(model, X, Y):
+    """
+    Plot the decision boundary given by model.
+    Parameters
+    ----------
+    model : model, whose parameters are used to plot the decision boundary.
+    X : input data
+    Y : input labels
+    """
+    x1_array, x2_array = np.meshgrid(np.arange(-4, 4, 0.01), np.arange(-4, 4, 0.01))
+    grid_coordinates = np.c_[x1_array.ravel(), x2_array.ravel()]
+    Z = model.predict(grid_coordinates)
+    Z = Z.reshape(x1_array.shape)
+    plt.contourf(x1_array, x2_array, Z, cmap=plt.cm.bwr)
+    plt.scatter(X[:, 0], X[:, 1], c=Y, cmap=plt.cm.bwr)
+    plt.show()
 
 
+def crossValidate(X_train, Y_train, X_test, Y_test, l_rate, epochs, reg_lambda, params, plot=False):
 
-def crossValidate(X_train, Y_train, X_test, Y_test, l_rate, epochs, reg_lambda, architecture):
-	model = train(X_train, Y_train, [l_rate, epochs, reg_lambda, architecture])
-	# plotDecisionBoundary(model, X_train, Y_train)
-	Y_pred = test(X_test,model)
-	pf = getPerformanceScores(Y_test,Y_pred)
-	print("Accuracy: {}\t Precision: {} \t Recall: {} \t F1: {}".format(pf["accuracy"],pf["precision"],pf["recall"],pf["f1"]))
-
-# def main():
-# 	k = 5
-# 	epochs = 10000
-# 	lr = 0.1
-# 	reg_lambda = 1
-# 	architecture = [2, 10, 1]
-# 	X,Y = getData(["./Data/DataFor640/dataset1","lin"])
-# 	K_split_data = splitData(X,Y,k)
-# 	count = 1
-# 	for itrain,itest in K_split_data:
-# 		print("Split " + str(count))
-# 		count += 1
-# 		X_Train, Y_Train = X[itrain],Y[itrain]
-# 		X_Test, Y_Test = X[itest],Y[itest]
-# 		crossValidate(X_Train, Y_Train, X_Test, Y_Test, lr, epochs, reg_lambda, architecture)
+	net = TwoLayerMLP(params[0],params[1],params[2],params[3],"sigmoid")
+	stats = net.train(X_train, Y_train,learning_rate=l_rate, reg=reg_lambda,  num_epochs=epochs, verbose=False)
+	output = net.predict(X_test)
+	if plot:
+		plotDecisionBoundary(net, X_train, Y_train)
+	print('Final training loss: ', stats['loss_history'][-1])
+	print("-----Confusion Matrix-----")
+	scores=getPerformanceScores(Y_test,output)
+	print("Overall Accuracy: ",scores["accuracy"])
+	for i in range(scores['CM'].shape[0]):
+		print("--Label ",i,"--")
+		print("Precision: ",scores["precision"][i])
+		print("Recall: ",scores["recall"][i])
+		print("F1-score: ",scores["f1"][i])
+	return stats
 
 def main():
+
+	print("-------Dataset 1:Linear-------")
 	X,Y = getData(["./Data/DataFor640/dataset1","lin"])
 
 	# Y_int = Y.astype(int)
@@ -36,26 +51,69 @@ def main():
 	count = 1
 	for itrain,itest in K_split_data:
 		print("Split " + str(count))
-		count += 1
 		X_Train, Y_Train = X[itrain],Y[itrain].astype(int)
 		X_Test, Y_Test = X[itest],Y[itest].astype(int)
 
-		net = TwoLayerMLP(X_Train.shape[1],3,2,5,"sigmoid")
-		stats = net.train(X_Train, Y_Train, X_Train, Y_Train,learning_rate=1.2, reg=1e-1,  num_epochs=100, verbose=True)
-		output = net.predict(X_Test)
-		print(getConfusionMatrix(Y_Test,output))
-		print('Final training loss: ', stats['loss_history'][-1])
+		stats = crossValidate(X_Train, Y_Train, X_Test, Y_Test, 1.2, 200, 1e-2, [X_Train.shape[1],3,2,5], True)
 
 		# plot the loss history and gradient magnitudes
 		plt.subplot(2, 1, 1)
 		plt.plot(stats['loss_history'])
 		plt.xlabel('epoch')
 		plt.ylabel('training loss')
-		plt.title('Training Loss history')
+		plt.title('Linear Training Loss history - Split '+str(count))
 		plt.show()
+
+		count += 1
+
+	print("-------Dataset 1:Non Linear-------")
+	X,Y = getData(["./Data/DataFor640/dataset1","nonlin"])
+
+	# Y_int = Y.astype(int)
+	  #     X.reshape(897*64,1)
+	K_split_data = splitData(X,Y)
+	count = 1
+	for itrain,itest in K_split_data:
+		print("Split " + str(count))
+		X_Train, Y_Train = X[itrain],Y[itrain].astype(int)
+		X_Test, Y_Test = X[itest],Y[itest].astype(int)
+
+		stats = crossValidate(X_Train, Y_Train, X_Test, Y_Test, 2, 300, 1e-3, [X_Train.shape[1],3,2,5], True)
+
+		# plot the loss history and gradient magnitudes
+		plt.subplot(2, 1, 1)
+		plt.plot(stats['loss_history'])
+		plt.xlabel('epoch')
+		plt.ylabel('training loss')
+		plt.title('Non Linear Training Loss history - Split '+str(count))
+		plt.show()
+
+		count += 1
+
+
+
+
+
+
+	print("-------Dataset 2:Digits-------")
+	X,Y = getData(["./Data/DataFor640/dataset2","digits"])
+	X_Train,X_Test = X[0],X[1]
+	Y_Train,Y_Test = Y[0].astype(int),Y[1].astype(int)
+	# net = TwoLayerMLP(X_Train.shape[1],10,150,2,"sigmoid")
+	stats = crossValidate(X_Train, Y_Train, X_Test, Y_Test, 1.5 , 250, 1e-2, [X_Train.shape[1],10,150,2])
+
+
+	# plot the loss history and gradient magnitudes
+	plt.subplot(2, 1, 1)
+	plt.plot(stats['loss_history'])
+	plt.xlabel('epoch')
+	plt.ylabel('training loss')
+	plt.title('Training Loss history')
+
+
 
 
 
 if __name__ == '__main__':
-	random.seed(0)
+	# random.seed(0)
 	main()
